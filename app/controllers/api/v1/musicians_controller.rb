249e086b1c5ec7c785b4285f73b7971576be52ad
Andrew Haveles <andxhav@gmail.com>
2019-05-13 15:00:42 -0400

    added feature to allow musician to dynamically change their state of open or closed

--- app/controllers/api/v1/musicians_controller.rb
+++ app/controllers/api/v1/musicians_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::MusiciansController < ApplicationController
   def show
     render json: Musician.find(params[:id])
   end
+
+  def create
+    musician = Musician.find(params[:musician_id])
+    if musician
+      musician.update(musician_state: params[:musician_state])
+    end
+  end
+
 end
